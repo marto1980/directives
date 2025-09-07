46d0b9ba804e2b9b9b8b85ee004150bb4a626fcc
Martin Aleksandrov Ivanov <marto1980@gmail.com>
2025-09-07 11:13:33 +0300

    Use AuthService to Define isAdmin

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,7 @@
-import { Component, signal } from '@angular/core'
+import { Component, computed, inject, signal } from '@angular/core'
 
 import { AuthComponent } from './auth/auth'
+import { AuthService } from './auth/auth.service'
 import { LearningResourcesComponent } from './learning-resources/learning-resources'
 
 @Component({
@@ -11,4 +12,9 @@ import { LearningResourcesComponent } from './learning-resources/learning-resour
 })
 export class App {
   protected readonly title = signal('directives')
+  private readonly authService = inject(AuthService)
+  private readonly checkIsAdmin = () =>
+    this.authService.activePermission() === 'admin'
+
+  isAdmin = computed(this.checkIsAdmin)
 }
